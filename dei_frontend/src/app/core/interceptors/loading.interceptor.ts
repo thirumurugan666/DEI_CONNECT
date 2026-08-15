6876bef8a3bf4d7584bc7fa497c0aec6127f2912
thirumurugan250 <Thirumurugan.S2@cognizant.com>
2026-08-15 07:36:16 +0530

    Dei connect full project

---
+++ dei_frontend/src/app/core/interceptors/loading.interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { finalize } from 'rxjs';
+
+import { LoadingService } from '../services/loading.service';
+import { API } from '../constants/api-paths';
+
+export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
+  const loading = inject(LoadingService);
+  loading.start();
+  return next(req).pipe(
+    finalize(() => {
+      loading.stop();
+    })
+  );
+};
